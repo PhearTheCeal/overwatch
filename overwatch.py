ad2bc88ad463c58e4fd048d002ed1fa4cccfb6dd
PTC <pheartheceal@gmail.com>
2016-09-20 23:24:05 -0700

    show pools instead of teams

--- overwatch.py
+++ overwatch.py
@@ -1,7 +1,9 @@
 """ Overwatch team finding tool. """
+# pylint: disable=too-many-branches,too-many-statements
 
 import itertools
 import argparse
+from collections import Counter
 from overcrawl import get_counters
 from overrank import get_rankings
 
@@ -47,10 +49,10 @@ SUPPORT = set(['lucio', 'symmetra', 'mercy', 'ana', 'zenyatta'])
 HEALERS = SUPPORT.difference(set(['symmetra']))
 DPS = OFFENSE.union(DEFENSE)
 
-JACOB = ['reaper', 'mccree', 'roadhog', 'dva', 'junkrat', 'reinhardt', 'bastion', 'zenyatta', 'lucio']
-KEVIN = ['bastion', 'soldier-76', 'reinhardt', 'mei', 'zenyatta', 'winston', 'zarya']
-DAVID = ['dva', 'roadhog', 'mercy', 'mccree', 'tracer']  # , 'soldier-76', 'lucio', 'pharah', 'ana']
-CRITTER = ['zenyatta', 'pharah', 'reinhardt', 'bastion', 'torbjorn']
+JACOB = set(['reaper', 'mccree', 'roadhog', 'dva', 'junkrat', 'reinhardt', 'bastion', 'zenyatta', 'lucio'])
+KEVIN = set(['bastion', 'soldier-76', 'reinhardt', 'mei', 'zenyatta', 'winston', 'zarya'])
+DAVID = set(['dva', 'roadhog', 'mercy', 'mccree', 'tracer', 'soldier-76', 'lucio', 'pharah', 'ana'])
+CRITTER = set(['zenyatta', 'pharah', 'reinhardt', 'bastion', 'torbjorn'])
 
 
 def main():
@@ -65,14 +67,19 @@ def main():
     args = parser.parse_args()
 
     player_choices = []
+    pick_pool = {}
     if args.jacob:
         player_choices.append(JACOB)
+        pick_pool['jacob'] = Counter()
     if args.kevin:
         player_choices.append(KEVIN)
+        pick_pool['kevin'] = Counter()
     if args.david:
         player_choices.append(DAVID)
+        pick_pool['david'] = Counter()
     if args.critter:
         player_choices.append(CRITTER)
+        pick_pool['critter'] = Counter()
     for random_hero in args.random:
         player_choices.append([random_hero])
 
@@ -82,13 +89,37 @@ def main():
                                           if len(set(s)) == len(s))
     possible_teams = list(k for k, _ in itertools.groupby(possible_teams))
 
+    print " ".join(possible_teams[0])
     for team in possible_teams:
-        team_set = set(team)
-        if any(len(team_set.intersection(role)) < 1 for role in (TANKS, HEALERS)):
+        if any(len(team.intersection(role)) < 1 for role in (TANKS, HEALERS)):
             continue
         weak = weakest_link(team)
-        print "{} --- WEAKEST LINK {} against {} {} --- POWER LEVEL {}".format(
-                ' '.join(sorted(team)), weak['hero'], weak['counter'], weak['winrate'], sum_rank(team))
+        if weak['winrate'] <= 0.5:
+            break
+        if args.jacob:
+            pick_pool['jacob'] += Counter(team & JACOB)
+        if args.david:
+            pick_pool['david'] += Counter(team & DAVID)
+        if args.kevin:
+            pick_pool['kevin'] += Counter(team & KEVIN)
+        if args.critter:
+            pick_pool['critter'] += Counter(team & CRITTER)
+    if args.jacob:
+        print 'Jacob'
+        for hero in sorted(pick_pool['jacob'], reverse=True, key=lambda n: pick_pool['jacob'][n]):
+            print '\t', hero, pick_pool['jacob'][hero]
+    if args.kevin:
+        print 'Kevin'
+        for hero in sorted(pick_pool['kevin'], reverse=True, key=lambda n: pick_pool['kevin'][n]):
+            print '\t', hero, pick_pool['kevin'][hero]
+    if args.david:
+        print 'David'
+        for hero in sorted(pick_pool['david'], reverse=True, key=lambda n: pick_pool['david'][n]):
+            print '\t', hero, pick_pool['david'][hero]
+    if args.critter:
+        print 'Critter'
+        for hero in sorted(pick_pool['critter'], reverse=True, key=lambda n: pick_pool['critter'][n]):
+            print '\t', hero, pick_pool['critter'][hero]
 
 
 if __name__ == '__main__':
